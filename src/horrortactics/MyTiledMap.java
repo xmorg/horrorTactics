61068f35ebc6390a213e9323bdfca6d378559093
Tim <jobs@cooperlabs.net>
2017-02-07 04:27:35 -0800

    more artwork

--- src/horrortactics/MyTiledMap.java
+++ src/horrortactics/MyTiledMap.java
@@ -86,7 +86,8 @@ public class MyTiledMap extends TiledMap {
         selected_tile_x = -1;
         selected_tile_y = -1;
         tiles250x129 = new Image("data/tiles250x129.png");
-        player = new Actor("data/player00", 218, 313);
+        //player = new Actor("data/player00", 218, 313);
+        player = new Actor("data/monster05", 218, 313); //monster test
         turn_order = "planning";
         this.active_trigger = new Trigger("none", "none");
         /*for (int i = 0; i < 5; i++) {
