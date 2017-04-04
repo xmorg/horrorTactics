e76d208721ec3da0855397c47782b2fa5633af9b
Tim <jobs@cooperlabs.net>
2017-04-03 23:30:58 -0700

    start scripts

---
+++ start.bat
@@ -0,0 +1,5 @@
+#/bin/bash
+#cp dist/horrorTactics.jar ./
+#cp natives/* dist/lib/
+#java -Djava.library.path=/home/tcooper/devel/slick -jar horrorTactics.jar -cp /home/tcooper/devel/slick horrortactics.HorrorTactics
+java -Djava.library.path=./lib -jar horrorTactics.jar -cp ./lib horrortactics.HorrorTactics
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 cp dist/horrorTactics.jar ./
-java -Djava.library.path=/home/tcooper/devel/slick -jar horrorTactics.jar -cp /home/tcooper/devel/slick horrortactics.HorrorTactics
+#cp natives/* dist/lib/
+#java -Djava.library.path=/home/tcooper/devel/slick -jar horrorTactics.jar -cp /home/tcooper/devel/slick horrortactics.HorrorTactics
+java -Djava.library.path=./lib -jar horrorTactics.jar -cp /home/tcooper/devel/slick horrortactics.HorrorTactics
