b3e045592989c7391c8b013389bf4e1aaf11274c
Tim Cooper <tcooper@bdimont.local>
2017-04-13 15:19:56 -0700

    new cinematics

--- src/horrortactics/Cutscene.java
+++ src/horrortactics/Cutscene.java
@@ -13,6 +13,17 @@ import org.newdawn.slick.Image;
  * @author tcooper
  */
 public class Cutscene {
-    Image[] background = new Image[5];
+    String mapname;
+    Image[] background = new Image[5]; /* 5 possible backgrounds */
+    Image[] l_character = new Image[5]; /* speaking character on the left*/
+    Image[] r_character = new Image[5]; /* speaking character on the right*/
+    String[] text_1 = new String[5]; /* text per slide */
+    String[] text_2 = new String[5]; /* text per scene */
+    String[] text_3 = new String[5]; /* text per scene */
     
+    public Cutscene(MyTiledMap map, String scenename) {
+        //super(scenename);
+        //get the name of the map
+        //match the name of the map with the data, and load the above.
+    }
 }
